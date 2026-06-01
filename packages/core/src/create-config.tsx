6d383bdcb539e14580875bb0054bf330fd73afda
Joey Finkel <61193959+joeyfinkel@users.noreply.github.com>
2026-06-01 12:28:15 -0400

    feat(core): implement defineResourceLayout factory

--- packages/core/src/create-config.tsx
+++ packages/core/src/create-config.tsx
@@ -1,68 +1,110 @@
 import type { JSX } from 'react';
 import {
+  ComposableComponents,
+  MakeComposable,
+  makeComposable,
+  MakeComposableOptions,
+} from './composable';
+import {
+  type AnyBuiltPropDefinition,
   createPrimitivePropBuilder,
   createProp,
-  EnumWrappedProp,
-  type AnyBuiltPropDefinition,
+  type ResolveLayoutProps,
+  type ResolveProps,
+  validateProps,
 } from './create-value';
 import {
+  IncludedProps,
+  InferredInProps,
+  InPropsDefinition,
+  InPropsObject,
+  LayoutRenderProps,
+} from './props';
+import {
   normalizeResources,
-  ResourceEnum,
   toResourceEnum,
   type NormalizeResources,
   type ResourceDefinition,
 } from './resource';
+import { BaseComponent, pick, Show } from './utils';
 
-type DefinedResourceLayout<
-  Resources extends ReadonlyArray<ResourceDefinition>,
-> = {
-  views: NormalizeResources<Resources>;
-};
-
-type InPropsObject = Record<string, AnyBuiltPropDefinition>;
-type InPropsFunction<Resources extends ReadonlyArray<ResourceDefinition>> =
-  (props: {
-    resource: EnumWrappedProp<ResourceEnum<Resources>, 'string', 'required'>;
-  }) => InPropsObject;
-type InPropsDefinition<Resources extends ReadonlyArray<ResourceDefinition>> =
-  | InPropsObject
-  | InPropsFunction<Resources>;
-type InferredInProps<
-  Resources extends ReadonlyArray<ResourceDefinition>,
-  Options extends InPropsDefinition<Resources>,
-> = Options extends InPropsFunction<Resources> ? ReturnType<Options> : Options;
-type IncludedProps<T> = {
-  [_ in keyof T]?: true;
-};
 type LayoutProps<
   Resources extends ReadonlyArray<ResourceDefinition>,
   Options extends InPropsDefinition<Resources>,
   IncludeProps extends IncludedProps<InferredInProps<Resources, Options>> = {},
   CustomProps extends InPropsObject = {},
 > = {
-  include?: IncludeProps; // map of created props from `options` with a `true` value to include in the layout
+  /**
+   * Props to include in the layout.
+   */
+  include?: IncludeProps;
   /**
    * Custom props that the layout will receive.
    */
   custom?: CustomProps;
 };
-type CreateLayoutOptions<
-  Resources extends ReadonlyArray<ResourceDefinition>,
-  Options extends InPropsDefinition<Resources>,
-  IncludeProps extends IncludedProps<InferredInProps<Resources, Options>> = {},
-  CustomProps extends InPropsObject = {},
-> = {
-  props?: LayoutProps<Resources, Options, IncludeProps, CustomProps>;
-  render: (
-    props: CustomProps &
-      Pick<InferredInProps<Resources, Options>, keyof IncludeProps & string>,
-  ) => JSX.Element;
+
+type LayoutRenderContext = {
+  composables: unknown;
+  inProps: Record<string, unknown>;
 };
+
+type SplitLayoutInPropDefinition<
+  Props extends InPropsObject = {},
+  Content = unknown,
+> = {
+  props?: Props;
+  render: ((props: ResolveProps<Props>) => Content) | Content;
+};
+
+function isBuiltPropDefinition(
+  value: unknown,
+): value is AnyBuiltPropDefinition {
+  return typeof value === 'function' && value !== null && 'visibility' in value;
+}
+
+function isSplitLayoutInPropDefinition(
+  value: unknown,
+): value is SplitLayoutInPropDefinition {
+  return (
+    value !== null &&
+    typeof value === 'object' &&
+    'render' in value &&
+    (value as { render?: unknown }).render !== undefined
+  );
+}
+
+function splitLayoutInProps(inProps: Record<string, unknown>) {
+  const resolvedInProps: Record<string, AnyBuiltPropDefinition> = {};
+  const splitInProps: Record<string, unknown> = {};
+
+  for (const [key, value] of Object.entries(inProps)) {
+    if (isBuiltPropDefinition(value)) {
+      resolvedInProps[key] = value;
+      continue;
+    }
+
+    if (isSplitLayoutInPropDefinition(value)) {
+      splitInProps[key] = value.render;
+
+      if (value.props && typeof value.props === 'object') {
+        Object.assign(resolvedInProps, value.props);
+      }
+    }
+  }
+
+  return {
+    resolvedInProps,
+    splitInProps,
+  };
+}
+
 type CreateViewMapOptions<
   Resources extends ReadonlyArray<ResourceDefinition>,
   Options extends InPropsDefinition<Resources>,
   IncludeProps extends IncludedProps<InferredInProps<Resources, Options>> = {},
   CustomProps extends InPropsObject = {},
+  Composables extends ComposableComponents = {},
 > = {
   /**
    * An array of valid resource names to support.
@@ -72,8 +114,72 @@ type CreateViewMapOptions<
    * The options that are passed into the created resource layout.
    */
   options: Options;
-  layout: CreateLayoutOptions<Resources, Options, IncludeProps, CustomProps>;
+  layout: {
+    /**
+     * The props to pass to the layout.
+     */
+    props?: LayoutProps<Resources, Options, IncludeProps, CustomProps>;
+    /**
+     * Components used to compose the layout.
+     */
+    composables?: Composables;
+    /**
+     * The render function for the layout.
+     */
+    render: (
+      props: LayoutRenderProps<Resources, Options, IncludeProps, CustomProps>,
+      context: LayoutRenderContext,
+    ) => JSX.Element;
+  };
 };
+
+type ResourceLayoutComposition<
+  Name extends string,
+  Composables extends ComposableComponents,
+> = [keyof Composables] extends [never]
+  ? {}
+  : {
+      makeComposable: MakeComposable<Composables, Name>;
+    };
+type ResourceLayoutComponent<
+  Name extends string,
+  Props extends InPropsObject = {},
+  Composables extends ComposableComponents = {},
+> = ResourceLayoutComposition<Name, Composables> &
+  BaseComponent<Name, ResolveProps<Props>> & {
+    (props: Show<ResolveProps<Props>>): JSX.Element;
+  };
+
+type DefinedResourceLayoutOptions<
+  Resources extends ReadonlyArray<ResourceDefinition>,
+  InProps extends InPropsDefinition<Resources>,
+  Name extends string,
+  Resource extends keyof NormalizeResources<Resources>,
+  Props extends InPropsObject = {},
+> = ResolveLayoutProps<InferredInProps<Resources, InProps>> & {
+  name: Name;
+  resource: Resource;
+  props?: Props;
+};
+type DefinedResourceLayout<
+  Resources extends ReadonlyArray<ResourceDefinition>,
+  InProps extends InPropsDefinition<Resources>,
+  IncludeProps extends IncludedProps<InferredInProps<Resources, InProps>> = {},
+  CustomProps extends InPropsObject = {},
+  Composables extends ComposableComponents = {},
+> = <
+  Name extends string,
+  Resource extends keyof NormalizeResources<Resources>,
+  Props extends InPropsObject = {},
+>(
+  options: DefinedResourceLayoutOptions<
+    Resources,
+    InProps,
+    Name,
+    Resource,
+    Props
+  >,
+) => ResourceLayoutComponent<Name, CustomProps, Composables>;
 
 const createResourceLayout = defineResourceLayout({
   resources: [
@@ -107,7 +213,7 @@ const createResourceLayout = defineResourceLayout({
         className: createProp.string().optional(),
       },
     },
-    render: ({}) => <></>,
+    render: () => <></>,
   },
 });
 createResourceLayout({
@@ -116,6 +222,7 @@ createResourceLayout({
   props: {
     segment: createProp.string(),
   },
+  title: () => <></>,
 });
 
 export function defineResourceLayout<
@@ -123,26 +230,98 @@ export function defineResourceLayout<
   InProps extends InPropsDefinition<Resources>,
   IncludeProps extends IncludedProps<InferredInProps<Resources, InProps>> = {},
   CustomProps extends InPropsObject = {},
+  Composables extends ComposableComponents = {},
 >(
-  options: CreateViewMapOptions<Resources, InProps, IncludeProps, CustomProps>,
+  options: CreateViewMapOptions<
+    Resources,
+    InProps,
+    IncludeProps,
+    CustomProps,
+    Composables
+  >,
 ) {
-  const { options: inProps, resources } = options;
+  const { options: inProps, resources, layout } = options;
   const normalizedResources = normalizeResources(resources);
-  const resourceProp = createPrimitivePropBuilder('string').enum(
+  const resourcesEnum = createPrimitivePropBuilder('string').enum(
     toResourceEnum(normalizedResources),
   );
-  const resolvedOptions =
-    typeof inProps === 'function'
-      ? inProps({ resource: resourceProp })
-      : inProps;
+  const createComposableLayout =
+    makeComposable<
+      LayoutRenderProps<Resources, InProps, IncludeProps, CustomProps>
+    >();
+  const definedResourceLayout: DefinedResourceLayout<
+    Resources,
+    InProps,
+    IncludeProps,
+    CustomProps,
+    Composables
+  > = (options) => {
+    const { name, props: instancePropDefinitions, ...layoutOptionProps } =
+      options;
+    const nameProp = createProp.string().literal(name);
+    const rawResolvedOptions =
+      typeof inProps === 'function'
+        ? inProps({
+            resource: resourcesEnum,
+            name: nameProp,
+          })
+        : inProps;
+    const { resolvedInProps, splitInProps } = splitLayoutInProps({
+      ...(rawResolvedOptions as Record<string, unknown>),
+      ...(instancePropDefinitions as Record<string, unknown> | undefined),
+      ...layoutOptionProps,
+    });
+    const { composables, render, props: layoutProps } = layout;
+    const customLayoutProps = layoutProps?.custom;
+    const includeLayoutProps = layoutProps?.include;
+    const resolvedIncludedProps = pick(
+      resolvedInProps,
+      Object.keys(includeLayoutProps ?? {}),
+    );
+    const resolvedLayoutProps = {
+      ...resolvedIncludedProps,
+      ...customLayoutProps,
+    };
+    const composableLayout = createComposableLayout({
+      components: composables,
+      name,
+    });
+    const mergedRenderContext: LayoutRenderContext = {
+      composables: composableLayout,
+      inProps: splitInProps,
+    };
 
-  return <
-    Name extends string,
-    Resource extends keyof NormalizeResources<Resources>,
-    Props extends InPropsDefinition<Resources> = {},
-  >(options: {
-    name: Name;
-    resource: Resource;
-    props?: Props;
-  }) => {};
+    function Component(props: Show<ResolveProps<CustomProps>>) {
+      const validatedProps = validateProps(resolvedLayoutProps, props);
+      const layoutRenderProps = {
+        ...validatedProps,
+        ...resolvedInProps,
+      } as unknown as LayoutRenderProps<
+        Resources,
+        InProps,
+        IncludeProps,
+        CustomProps
+      >;
+
+      return <>{render(layoutRenderProps, mergedRenderContext)}</>;
+    }
+
+    return Object.assign(Component, {
+      displayName: name,
+      props: undefined as unknown as ResolveProps<CustomProps>,
+      ...createComposition({ components: composables, name }),
+    });
+  };
+
+  function createComposition<Name extends string>(
+    options: MakeComposableOptions<Composables, Name>,
+  ): ResourceLayoutComposition<Name, Composables> {
+    if (!options.components) {
+      return {} as ResourceLayoutComposition<Name, Composables>;
+    }
+
+    return { makeComposable: createComposableLayout(options) };
+  }
+
+  return definedResourceLayout;
 }
